08e9f67ecee8937d72ca7c8fe15547e8ef12e6b0
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2020-10-02 09:55:14 +0100

    rename to differential2legacy

--- src/adjoint.jl
+++ src/adjoint.jl
@@ -8,24 +8,29 @@ typeless(x) = MacroTools.postwalk(x -> isexpr(x, :(::), :kw) ? x.args[1] : x, x)
 isvararg(x) = isexpr(x, :(::)) && namify(x.args[2]) == :Vararg
 
 """
-    generic2diff(x)
+    legacy2differential(x)
 
 Convert input `x` from the legacy ZygoteRules format to the ChainRules differential types.
 """
-generic2diff(x) = x
-generic2diff(::Nothing) = Zero()
-generic2diff(t::Union{Tuple, NamedTuple}) = map(generic2diff, t)
+legacy2differential(x) = x
+legacy2differential(::Nothing) = Zero()
+legacy2differential(t::Union{Tuple, NamedTuple}) = map(legacy2differential, t)
 
 """
-    diff2generic(x)
+    differential2legacy(x)
 
 Convert input `x` from the ChainRules differential types to the legacy ZygoteRules format.
 """
-diff2generic(x) = x
-diff2generic(::AbstractZero) = nothing
-diff2generic(t::Union{Tuple, NamedTuple}) = map(diff2generic, t)
-function diff2generic(::Nothing)
-    @error "Zygote internal use  of 'nothing', rather than `AbstractZero`, detected.  This should never occur. Please open an issue on https://github.com/FluxML/Zygote.jl/issues, including the full text of this message"  stacktrace=stacktrace()
+differential2legacy(x) = x
+differential2legacy(::AbstractZero) = nothing
+differential2legacy(t::Union{Tuple, NamedTuple}) = map(differential2legacy, t)
+function differential2legacy(::Nothing)
+    # can't use logging macros as that breaks nested AD.
+    println("Zygote internal use  of 'nothing', rather than `AbstractZero`, detected.  This should never occur. Please open an issue on https://github.com/FluxML/Zygote.jl/issues, including the full text of this message. \n Stacktrace:")
+    for (ii, callsite) in enumerate(stacktrace())
+      ii > 10 && break
+      println("[$ii] $callsite")
+    end
     return nothing
 end
 
@@ -69,18 +74,18 @@ function gradm(ex, mut = false)
     @inline function ZygoteRules._pullback($cx, $f::$T, $(args...)) where $(Ts...)
       y, _back = adjoint(__context__, $f, $(argnames...))
       $(mut ? nothing : :(back(::Union{Nothing,AbstractZero}) = Zero()))
-      #back(Δ) = $gradtuple(generic2diff(_back(diff2generic(Δ))))
-      back(Δ) = $gradtuple(generic2diff(_back(Δ)))
+      back(Δ) = $gradtuple(legacy2differential(_back(differential2legacy(Δ))))
+      #back(Δ) = $gradtuple(legacy2differential(_back(Δ)))
       return y, back
     end
     @inline function ZygoteRules._pullback($cx, ::$kT, kw, $f::$T, $(args...)) where $(Ts...)
       y, _back = adjoint(__context__, $f, $(argnames...); kw...)
       $(mut ? nothing : :(back(::Union{Nothing,AbstractZero}) = Zero()))
-      #back(Δ) = $gradtuplekw(generic2diff(_back(diff2generic(Δ))))
-      back(Δ) = $gradtuplekw(generic2diff(_back(Δ)))
+      back(Δ) = $gradtuplekw(legacy2differential(_back(differential2legacy(Δ))))
+      #back(Δ) = $gradtuplekw(legacy2differential(_back(Δ)))
       return y, back
     end
-    nothing # why is this here?
+    return nothing  # make nothing show in terminal after using macro
   end
 end
 
